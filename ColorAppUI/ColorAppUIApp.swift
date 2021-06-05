27d6ba72024b15ab3abca8ee56f915cee7e00cb6
Artem Bazhanov <abazhanov@yahoo.com>
2021-06-05 11:33:50 +0300

    Initial Commit

---
+++ ColorAppUI/ColorAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorAppUIApp.swift
+//  ColorAppUI
+//
+//  Created by Artem Bazhanov on 05.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
